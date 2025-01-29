94419731fd5aefcbd5a5fa207d9c1162c355c392
shiro <shiro.ishii@sesattlecolleges.edu>
2025-01-29 02:22:15 -0800

    created JS module called data.js

---
+++ HW2/data.js
@@ -0,0 +1,38 @@
+const myShowcase = [
+    {
+      model: "Miata",
+      make: "Mazda",
+      year: 1994,
+      color: "blue",
+      mileage: 100000
+    },
+    {
+      model: "Miata",
+      make: "Mazda",
+      year: 1995,
+      color: "black",
+      mileage: 120000
+    },
+    {
+      model: "Prius",
+      make: "Toyota",
+      year: 2023,
+      color: "grey",
+      mileage: 20000
+    },
+    {
+      model: "Land Cruiser",
+      make: "Toyota",
+      year: 1997,
+      color: "black",
+      mileage: 85000
+    },
+    {
+      model: "Outback",
+      make: "Subaru",
+      year: 2008,
+      color: "grey",
+      mileage: 100000
+    }
+  ];
+  
--- HW2/index.js
+++ HW2/index.js
@@ -1,3 +1,42 @@
+'use strict'
+import express from 'express';
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('./public')); // set location for static files
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+// send static file as response
+app.get('/', (req,res) => {
+    res.type('text/html');
+    res.sendFile('.public/index.html');
+  });
+  
+  // send plain text response
+  app.get('/about', (req,res) => {
+   res.type('text/plain');
+   res.send('About page');
+  });
+  
+
+  app.get('/user/:name', (req,res) => {
+    res.type('text/plain');
+    res.send(`Your name is ${req.params.name}`);
+  });
+  
+
+  // define 404 handler
+  app.use((req,res) => {
+   res.type('text/plain'); 
+   res.status(404);
+   res.send('404 - Not found');
+  });
+
+  app.listen(app.get('port'), () => {
+    console.log('Express started'); 
+  });
+
+/*
 import http from 'http';
 http.createServer((req,res) => {
     var path = req.url.toLowerCase();    
@@ -16,3 +55,4 @@ http.createServer((req,res) => {
             break;
     }    
 }).listen(process.env.PORT || 3000);
+*/
